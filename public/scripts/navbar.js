b31d1b11fb3e1620226dbe21d329aa825b790038
nikothomas <nikojb@gmail.com>
2024-08-27 03:00:37 -0700

    better mobile view

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -2,16 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const header = document.querySelector('header');
     const navbarLogo = document.querySelector('.nav-logo img');
     const profileIcon = document.getElementById('profile-icon');
+    const profileContainer = document.getElementById('profile-container');
 
     function handleScroll() {
         if (window.scrollY > 50) {
             header.classList.add('shrink');
             navbarLogo.classList.add('shrink');
             profileIcon.classList.add('shrink');
+            profileContainer.classList.add('shrink');
         } else {
             header.classList.remove('shrink');
             navbarLogo.classList.remove('shrink');
             profileIcon.classList.remove('shrink');
+            profileContainer.classList.remove('shrink');
         }
     }
 
